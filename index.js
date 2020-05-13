8d9dafd715386697f88bbb5c6e1240601adfeb17
Raphael Guimarães <raphaguimaraes@gmail.com>
2020-05-13 14:59:20 -0300

    chore: add commander

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 #!/usr/bin/env node
 
+const [,, ...args] = process.argv;
+const pkg = require("./package");
+
 const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
+
+const commander = require("commander");
 
 const files = require("./lib/files");
 
 clear();
 
 console.log(
-	chalk.blueBright(
-		figlet.textSync(
-			"WoozaCLI", { horizontalLayout: "default" }))
+	chalk.blueBright(figlet.textSync("CLI", { horizontalLayout: "default" }))
 );
 
-if (files.directoryExists(".git")) {
-	console.log(chalk.red("Already a Git repository!"));
-	process.exit();
-}
+commander
+	.version(pkg.version, "-v, --version")
+	.description("output the current version")
+	.parse(process.argv);
